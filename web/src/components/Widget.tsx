02727ab4f31725e6f2e57c6fb91107f5b8f2439f
Thigo <thiago.l17@yahoo.com>
2022-05-03 23:05:05 -0300

    addin useState

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,4 +1,22 @@
+import { ChatTeardropDots} from 'phosphor-react';
+import { useState } from 'react';
+
+
 export function Widget() {
-  return <h1>Widget</h1>
+  const [] = useState()
+
+  return (
+   <div className='absolute bottom-4 right-5'>
+     <p>Hello Word!</p>
+
+      <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center'>
+      <ChatTeardropDots className='w-6 h-6'/>
+
+      <span className='max-w-0 overflow-hidden group-hover; max-w-xs transition-all duration-500 group'>
+        <span>Feedback</span> 
+      </span>
+    </button>
+   </div>
+  )
 }
 
