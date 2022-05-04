7e81a63405fdcdc6dc04962e18e2393cf09cab21
Thigo <thiago.l17@yahoo.com>
2022-05-03 23:20:49 -0300

    addiing widget

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -3,17 +3,23 @@ import { useState } from 'react';
 
 
 export function Widget() {
-  const [] = useState()
+  const [isWidgetOpen, setIsWidgegetOpen] = useState(false)
+    
+  function toggleWidgetVisibility() {
+    setIsWidgegetOpen(isWidgetOpen)
+  }
 
   return (
-   <div className='absolute bottom-4 right-5'>
+   <div className='absolute bottom-5 right-5'>
      <p>Hello Word!</p>
 
-      <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center'>
+      <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
       <ChatTeardropDots className='w-6 h-6'/>
 
-      <span className='max-w-0 overflow-hidden group-hover; max-w-xs transition-all duration-500 group'>
-        <span>Feedback</span> 
+      <span className='max-w-0 overflow-hidden group-hover; max-w-xs transition-all duration-500 ease-linear'>
+        <span className='pl-2'>
+          Feedback
+          </span> 
       </span>
     </button>
    </div>
