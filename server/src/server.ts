f7c1d06e82bcb7148b44c2a1a5e45ad14ccffaa6
Thigo <thiago.l17@yahoo.com>
2022-05-07 16:47:38 -0300

    send e-mail

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (req, res) => {
-  return res.send('hello world!')
-})
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  });
+
+  return res.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () =>{
   console.log('HTTP Server Running!! 🔥🔥');
