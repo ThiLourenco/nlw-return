d4d12ceee078ae4e3c7f1f80760ffe37c00eb313
Thigo <thiago.l17@yahoo.com>
2022-05-06 22:12:14 -0300

    adding Widget Form and ajuste popover adding flex

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,10 +1,11 @@
 import { ChatTeardropDots} from 'phosphor-react';
 import { Popover } from '@headlessui/react';
+import { WidgetFrom } from './WidgetForm';
 
 export function Widget() {
   return (
-   <Popover className='absolute bottom-5 right-5'>
-     <Popover.Panel>Hello Word!</Popover.Panel>
+   <Popover className='absolute bottom-5 right-5 flex flex-col items-end'>
+     <Popover.Panel><WidgetFrom /></Popover.Panel>
 
       <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
       <ChatTeardropDots className='w-6 h-6'/>
