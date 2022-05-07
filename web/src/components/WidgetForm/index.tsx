f8332fb19ca2835848ca398e852833a7ab345b6c
Thigo <thiago.l17@yahoo.com>
2022-05-07 01:49:56 -0300

    adding form and button with styles tailwindcss

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { CloseButton } from "../CloseButton";
 
-import bugImagemUrl from '../assets/bug.svg';
-import ideaImagemUrl from '../assets/idea.svg';
-import thoughtImagemUrl from '../assets/thought.svg';
+import bugImagemUrl from '../../assets/bug.svg';
+import ideaImagemUrl from '../../assets/idea.svg';
+import thoughtImagemUrl from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: 'Problema',
     image: {
@@ -30,35 +32,24 @@ const feedbackTypes = {
   },
 };
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetFrom() {
-  const [feedbackType, setfeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+  
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
+      
       {!feedbackType ? (
-        <div className="flex py-8 gap-8 w-full">
-        { Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button
-            key={key} // id distinct images
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none " //effect border transparent with hover and acessibility
-              onClick={() => setfeedbackType(key as FeedbackType)}
-              type="button"
-            >
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          ) 
-        }) }
-      </div>
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hello World</p>
+        <FeedbackContentStep
+         feedbackType={feedbackType}
+         onFeedbackRestartRequested={handleRestartFeedback} />
       )}
 
       <footer className="text-xs text-neutral-400">
