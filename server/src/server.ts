64c883d79bf17cc7e0ca73bfe72f21bfef4593dd
Thigo <thiago.l17@yahoo.com>
2022-05-07 13:24:47 -0300

    create project with database using orm prisma

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+  return res.send('hello world!')
+})
+
+app.listen(3333, () =>{
+  console.log('HTTP Server Running!! 🔥🔥');
+});
+
+
